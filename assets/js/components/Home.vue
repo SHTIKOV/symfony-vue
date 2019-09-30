3be038605c10e8d61ead70ddfa12f807376b7ef6
shtykov <konstantine.shtikov@yandex.ru>
2019-09-30 13:36:42 +0500

    Fix wellcome page

--- assets/js/components/Home.vue
+++ assets/js/components/Home.vue
@@ -1,12 +1,16 @@
 <template>
     <div>
         <div class="jumbotron">
-            <h1 class="display-4">Добро пожаловать!</h1>
-            <p class="lead">Это простая сборка SPA в стеке Symfony + Vue.</p>
+            <img src="https://symfony.com/images/logos/header-logo.svg" class="rounded mx-auto d-block" alt="Symfony">
+            <img src="https://vuejs.org/images/logo.png" class="rounded mx-auto d-block" alt="Vue" style="height: 210px;">
+            <h1 class="display-4">Wellcome to your SPA application</h1>
+            <p class="lead">Easy to build application on stack: <code>Symfony 4.x</code> + <code>Vue</code> + <code>Vuex</code> + <code>Webpack</code> + <code>Sass</code>.</p>
             <hr class="my-4">
-            <p>Symfony + Vue + Vuex + SPA</p>
+            <p class="lead">SHTIKOV's project</p>
             <p class="lead">
-                 <router-link to="/" class="btn btn-primary btn-lg" >Главная</router-link>
+                <a href="https://packagist.org/packages/shtikov/symfony-vue" 
+                    class="btn btn-primary btn-lg"
+                    target="_blank">Documentation</a>
             </p>
         </div>
     </div>
@@ -19,5 +23,11 @@
 </script>
 
 <style scoped>
-
+    b {
+        font-weight: 700;
+    }
+    .jumbotron {
+        text-align: center;
+        background-color: #fff;
+    }
 </style>
--- assets/js/components/TopBar.vue
+++ assets/js/components/TopBar.vue
@@ -1,19 +1,27 @@
 <template>
     <div>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <router-link to="/" class="navbar-brand" >Symfony 4.x + Vue</router-link>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <router-link to="/" class="nav-link" >Главная</router-link>
-                    </li>
-                </ul>
-            </div>
-    </nav>
+        <div class="nav-wrapper">
+            <ul class="nav nav-pills justify-content-center">
+                <li class="nav-item">
+                    <router-link class="nav-link active" to="/">Home</router-link>
+                </li>
+                <li class="nav-item">
+                    <a href="https://packagist.org/packages/shtikov/symfony-vue" 
+                        class="nav-link"
+                        target="_blank">Documentation</a>
+                </li>
+                <li class="nav-item">
+                    <a href="https://github.com/SHTIKOV/symfony-vue" 
+                        class="nav-link"
+                        target="_blank">Repository</a>
+                </li>
+                <li class="nav-item">
+                    <a href="https://github.com/SHTIKOV" 
+                        class="nav-link"
+                        target="_blank">Author</a>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -24,5 +32,7 @@
 </script>
 
 <style scoped>
-
+    .nav-wrapper {
+        padding: 10px;
+    }
 </style>
